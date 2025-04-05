99ccab7af5828b1ab3bccfbae9685da1f4336d45
Lukasz Swierkowski <G00439343@atu.ie>
2025-04-05 19:14:09 +0100

    week 02

---
+++ week02/addOne.py
@@ -0,0 +1,7 @@
+# addOne.py
+# reading on a number and prints out more then one
+# Author: Lukasz Swierkowski
+
+number = int(input("enter a number: "))
+newNumber = number + 1
+print (f'{number} plus one is {newNumber}')
---
+++ week02/hello2.py
@@ -0,0 +1,6 @@
+# hello2.py
+# say my name
+# Author: Lukasz Swierkowski
+
+name = input ("Enter your name:")
+print ('Hello' + name + '\nNice to meet you')
